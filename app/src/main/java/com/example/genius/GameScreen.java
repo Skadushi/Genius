72c8a6d9e14222696d678c9a6469a95524cd3c22
Skadushi <andrei_deniz22@outlook.com>
2019-06-14 17:45:11 -0300

    onRestart showing buttons fixed

--- app/src/main/java/com/example/genius/GameScreen.java
+++ app/src/main/java/com/example/genius/GameScreen.java
@@ -3,11 +3,14 @@ package com.example.genius;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 
 public class GameScreen extends AppCompatActivity {
 
     private LinearLayout gameLayout;
+    private Button backArrow;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +22,26 @@ public class GameScreen extends AppCompatActivity {
         gameLayout.setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_FULLSCREEN);
         overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
 
+        backArrow = findViewById(R.id.backArrow);
+        backArrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gameLayout.setBackgroundResource(R.color.colorSecondary);
+                onBackPressed();
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        overridePendingTransition(android.R.anim.slide_in_left, android.R.anim.slide_out_right);
+        finish();
+    }
+
+    @Override
+    protected void onRestart() {
+        gameLayout.setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_FULLSCREEN);
+        super.onRestart();
     }
 }
--- app/src/main/java/com/example/genius/MainActivity.java
+++ app/src/main/java/com/example/genius/MainActivity.java
@@ -10,7 +10,7 @@ import android.widget.LinearLayout;
 public class MainActivity extends AppCompatActivity {
 
     private LinearLayout mainLayout;
-    private Button playSwap;
+    private Button playMenuButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,13 +22,19 @@ public class MainActivity extends AppCompatActivity {
         mainLayout.setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_FULLSCREEN);
         overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
 
-        playSwap = findViewById(R.id.playSwap);
+        playMenuButton = findViewById(R.id.playMenuButton);
 
-        playSwap.setOnClickListener(new View.OnClickListener() {
+        playMenuButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(v.getContext(), GameScreen.class));
             }
         });
+    }
+
+    @Override
+    protected void onRestart() {
+        mainLayout.setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_FULLSCREEN);
+        super.onRestart();
     }
 }
